9fd319b00860ab9d96c737fbefbf1dca7d87ccbc
AdarshNaidu <naidu.adarsh2511@gmail.com>
2020-03-13 15:57:38 +0530

    user model setup

---
+++ src/database/models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    password: {
+        type: String,
+    },
+})
+
+userSchema.plugin(passportLocalMongoose, { usernameField : 'email' });
+
+const User = new mongoose.model('User', userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+module.exports = User;
