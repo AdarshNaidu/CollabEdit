dd9658414e872fad1f236048cf60847f6d658dc0
AdarshNaidu <naidu.adarsh2511@gmail.com>
2020-03-23 12:16:39 +0530

    editor route looking up the saved document and rendering editor view

--- src/routes/editor.js
+++ src/routes/editor.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const Document = require('../database/models/document')
 
-router.get('/editor/:title', (req, res) => {
-    res.render('editor', {
-        title: req.params.title
-    })
+router.get('/editor/:title', async (req, res) => {
+    if(req.params.title.match(/^[0-9a-fA-F]{24}$/)){
+        const document = await Document.findById(req.params.title)
+        res.render('editor', {
+            title: req.params.title,
+            name: document.name
+        })
+    }else {
+        res.render('editor', {
+            title: req.params.title
+        })
+    }
+})
+
+router.get('/editor/:user/:title', (req, res) => {
+    if(req.user){
+        res.render('editor', {
+            title: req.params.title,
+            user: req.params.user
+        })
+    }
 })
 
 module.exports = router
