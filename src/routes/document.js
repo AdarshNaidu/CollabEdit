8e7773e90c31ffc02b8a9b279243e28a0679a900
AdarshNaidu <naidu.adarsh2511@gmail.com>
2020-03-23 12:14:40 +0530

    sending documents and deleting document route

--- src/routes/document.js
+++ src/routes/document.js
@@ -15,4 +15,22 @@ router.post('/document', (req, res) => {
     }
 })
 
+router.get('/document', async (req, res) => {
+    if(req.user){
+        const documents = await Document.find({owner: req.user._id})
+        // console.log(documents)
+        res.send(documents)
+    }
+})
+
+router.get('/document/delete/:id', async(req, res) => {
+    if(req.user){
+        const document = await Document.findById(req.params.id)
+        console.log('found')
+        if(document.owner.equals(req.user._id)){
+            res.send(await Document.findByIdAndDelete(req.params.id))
+        }else res.send('invalid')
+    }
+})
+
 module.exports = router;
